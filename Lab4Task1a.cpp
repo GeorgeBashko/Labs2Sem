d839e50b6c6f6cbf20170b857644646220f5e0a7
GeorgeBashko <90065629+GeorgeBashko@users.noreply.github.com>
2022-03-25 20:34:42 +0300

    Update Lab4Task1a.cpp

--- Lab4Task1a.cpp
+++ Lab4Task1a.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <math.h>
+using namespace std;
 int main()
 {
-	double x;
+	double arr[2];
+	double x, x1, x2;
 	double t1 = -10;
 	double t2 = 10;
 	double t = 0;
@@ -13,6 +15,8 @@ int main()
 	double z = 0.0;
 	double two = 2.0;
 	double t3, t4, t5, t6;
+	int kk = 0;
+	double ten = 10;
 	std::cout << "Enter a: ";
 	std::cin >> a;
 	std::cout << "Enter b: ";
@@ -23,94 +27,118 @@ int main()
 	{
 	_next:
 		finit
-		fstp t6
+			fstp t6
 			//середина
-		fld t1
-		fadd t2
-		fdiv two
-		fstp t
-		fld t
+			fld t1
+			fadd t2
+			fdiv two
+			fstp t
+			fld t
 			//значение первом на конце
-		mov ecx, 1
-		fld t1
-	beg :
+			mov ecx, 1
+			fld t1
+			beg :
 		fmul t1
-		loop beg
-		fmul a
-		fstp t3
-		fld t1
-		fadd el
-		fstp cur
-		fld b
-		fdiv cur
-		fstp cur
-		fld t3
-		fadd cur
-		fstp cur
-		fld c
-		fadd cur
-		fstp cur
+			loop beg
+			fmul a
+			fstp t3
+			fld t1
+			fadd el
+			fstp cur
+			fld b
+			fdiv cur
+			fstp cur
+			fld t3
+			fadd cur
+			fstp cur
+			fld c
+			fadd cur
+			fstp cur
 			//значеие на втором конце
-		mov ecx, 1
-		fld t
-	beg1 :
+			mov ecx, 1
+			fld t
+			beg1 :
 		fmul t
-	loop beg1
-		fmul a
-		fstp t4
-		fld t
-		fadd el
-		fstp cur1
-		fld b
-		fdiv cur1
-		fstp cur1
-		fld t4
-		fadd cur1
-		fstp cur1
-		fld c
-		fadd cur1
-		fstp cur1
+			loop beg1
+			fmul a
+			fstp t4
+			fld t
+			fadd el
+			fstp cur1
+			fld b
+			fdiv cur1
+			fstp cur1
+			fld t4
+			fadd cur1
+			fstp cur1
+			fld c
+			fadd cur1
+			fstp cur1
 			//произведение
-		fld cur
-		fmul cur1
-		fstp cur2
-		fld  cur2
-		fcom z
-		fstsw ax
-		sahf
-		jc  _less
-		jnz _great
-		jz _great
-	_less :
-		fstp t6
+			fld cur
+			fmul cur1
+			fstp cur2
+			fld  cur2
+			fcom z
+			fstsw ax
+			sahf
+			jc  _less
+			jnz _great
+			_zero :
+		fld cur1
+			fcom z
+			fstsw ax
+			sahf
+			jz zero
+			jmp zero1
+			zero :
 		fld t
-		fstp t2
-		jmp end
-	_great :
-		fstp t6
-		fld t
-		fstp t1
-	end :
-		fld t2
-		fsub t1
-		fstp rez
-		fabs
-		fld  rez
-		fcom pogr
-		fstsw ax
-		sahf
-		jc  end1
-		jnz _next
-	end1 :
+			fstp x2
+	}
+	arr[kk] = x2;
+	__asm
+	{
+		inc kk
+		jmp _less
+		zero1 :
 		fld t1
-		fadd t2
-		fdiv two
-		fstp x
-		mov ecx, 1
-		fld x
-		beg2 :
-		    fmul x
-	    loop beg2
+			fstp x2
+	}
+	arr[kk] = x2;
+	__asm
+	{
+		inc kk
+		jmp _less
+		_less :
+		fstp t6
+			fld t
+			fstp t2
+			jmp end2
+			_great :
+		fstp t6
+			fld t
+			fstp t1
+			end2 :
+		fld t2
+			fsub t1
+			fstp rez
+			fabs
+			fld  rez
+			fcom pogr
+			fstsw ax
+			sahf
+			jc  end1
+			jnz _next
+			end1 :
+		fld t1
+			fadd t2
+			fdiv two
+			fstp x
+			mov ecx, 1
+			fld x
+			beg2 :
+		fmul x
+			loop beg2
 			fmul a
 			fstp t5
 			fld x
@@ -132,13 +160,33 @@ int main()
 			jc  _less1
 			jnz _great1
 			je _great1
+			_less1 :
+		fld x
+			fstp x1
+	}
+	arr[kk] = x1;
+	__asm
+	{
+		inc kk
+		fld z
+		fstp t1
+		fld ten
+		fstp t2
+		mov eax, kk
+		sub eax, 2
+		cmp eax, 0
+		je _less2
+		jmp _next
 	}
 _great1:
 	{
 		std::cout << "Not roots" << std::endl;
 		return 0;
 	}
-_less1:
-	std::cout << "Root: " << x << std::endl;
+_less2:
+	for (int i = 0; i < kk; i++)
+	{
+		std::cout << "Root: " << arr[i] << std::endl;
+	}
 	return 0;
 }
