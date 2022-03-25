91494ac2613ba7f7e0c463f67a12ae840a86373a
GeorgeBashko <90065629+GeorgeBashko@users.noreply.github.com>
2022-03-25 20:42:05 +0300

    Update Lab4Task2b.cpp

--- Lab4Task2b.cpp
+++ Lab4Task2b.cpp
@@ -3,7 +3,7 @@
 #include<iomanip>
 int main()
 {
-    double pogr = 0.0000001;
+    double pogr = 0.00001;
     double k = 1;
     double rez1 = 0, rez = 0;
     double cur = 0;
@@ -13,14 +13,16 @@ int main()
     double kv = sqrt(8);
     const double pi = 3.141592653589793239;
     double x;
+    for (int i = 1; i <3; i++)
+    {
         __asm
         {
         beg:
-            finit
-            fstp x
-            fld rez1
-            fstp rez
-            fld k
+                finit
+                fstp x
+                fld rez1
+                fstp rez
+                fld k
                 fmul t
                 fsub ed
                 fstp cur
@@ -52,11 +54,14 @@ int main()
                 fstsw ax
                 sahf
                 jc  _less1
-                jmp beg            
+                jmp beg
         }
     _less1:
-        std::cout << "Number of iterations to find " << 5 << " correct digits after the decimal point: "<<k << std::endl;
-        std::cout << "Number of iterations to find " << 7 << " correct digits after the decimal point: " << 3.1831e+06 << std::endl;
-     std::cout << "Number of iterations to find " << 8<< " correct digits after the decimal point: " <<  3.2377e+07 << std::endl;
+        std::cout << "Number of iterations to find " << 2*i+3 << " correct digits after the decimal point: " << k << std::endl;
+        pogr /= 100;
+        rez = 0;
+        rez1 = 0;
+        k = 1;
+    }
     return 0;
 }
